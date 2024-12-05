17c15517c21c373dd0f2fe28fc61a7e82c3041eb
German Escobar <german.e.escobar@gmail.com>
2024-12-04 22:15:42 -0300

    env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,17 @@ import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { ExternalApiService } from './external-api/external-api.service';
 import { ExternalApiController } from './external-api/external-api.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    HttpModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [ExternalApiController],
   providers: [ExternalApiService],
-  exports: [ExternalApiService]
+  exports: [ExternalApiService],
 })
 export class AppModule {}
--- src/external-api/external-api.service.ts
+++ src/external-api/external-api.service.ts
@@ -1,27 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { firstValueFrom } from 'rxjs';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class ExternalApiService {
-    constructor(private readonly httpService: HttpService) {}
+  constructor(
+    private readonly httpService: HttpService,
+    private configService: ConfigService,
+  ) {}
 
   async sendData(bodyData: any) {
-    // Ejemplo usando JSONPlaceholder
+    const apiUrl = this.configService.get<string>('OLLAMA_API_URL');
+
     const { data } = await firstValueFrom(
-        this.httpService.post('http://localhost:11434/api/generate', bodyData)
+      this.httpService.post(`${apiUrl}/api/generate`, bodyData),
     );
     return data;
   }
-    
+
   async handleWebhook(payload: any) {
     try {
-        console.log('Webhook recibido:', payload);
+      console.log('Webhook recibido:', payload);
 
-        return payload.hub_challenge;
+      return payload.hub_challenge;
     } catch (error) {
-        console.error('Error procesando webhook:', error);
-        throw error;
+      console.error('Error procesando webhook:', error);
+      throw error;
     }
-}
+  }
 }
