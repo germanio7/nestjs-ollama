665d89f16598bc9e3888503b5da4cad9679e9089
German Escobar <german.e.escobar@gmail.com>
2024-12-05 20:54:17 -0300

    send whatsapp

--- src/external-api/external-api.service.ts
+++ src/external-api/external-api.service.ts
@@ -1,15 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
-import { firstValueFrom } from 'rxjs';
+import { firstValueFrom, lastValueFrom } from 'rxjs';
 import { ConfigService } from '@nestjs/config';
-import { log } from 'console';
 
 @Injectable()
 export class ExternalApiService {
   constructor(
     private readonly httpService: HttpService,
     private configService: ConfigService,
-  ) { }
+  ) {}
 
   async sendData(bodyData: any) {
     const apiUrl = this.configService.get<string>('OLLAMA_API_URL');
@@ -22,8 +21,6 @@ export class ExternalApiService {
 
   async subscriptionWebhook(payload: any) {
     try {
-      console.log("payload", payload);
-
       return payload['hub.challenge'];
     } catch (error) {
       console.error('Error procesando webhook:', error);
@@ -33,23 +30,63 @@ export class ExternalApiService {
 
   async receiveWebhook(payload: any) {
     try {
-      if (payload.entry[0].changes[0].value.messages[0].type == 'text') {
+      if (
+        payload.entry[0].changes[0].value.messages &&
+        payload.entry[0].changes[0].value.messages[0].type == 'text'
+      ) {
+        const from = payload.entry[0].changes[0].value.messages[0].from;
         const message = payload.entry[0].changes[0].value.messages[0].text.body;
-        console.log(message);
 
         const result = await this.sendData({
           model: 'llama3.2',
           prompt: message,
-          stream: false
+          stream: false,
         });
 
-        console.log(result.response);
+        const resultSendWhatsapp = await this.sendWhatsapp(
+          from,
+          result.response,
+        );
       }
 
       return;
     } catch (error) {
       console.error('Error procesando webhook:', error);
-      throw error;
+      return;
+    }
+  }
+
+  async sendWhatsapp(phone_number: string, message: string) {
+    try {
+      phone_number = phone_number.substring(3);
+
+      const apiUrl = this.configService.get<string>('WHATSAPP_URL');
+      const token = this.configService.get<string>('WHATSAPP_TOKEN');
+
+      const params = {
+        messaging_product: 'whatsapp',
+        recipient_type: 'individual',
+        to: '54' + phone_number,
+        type: 'text',
+        text: {
+          preview_url: false,
+          body: message,
+        },
+      };
+
+      const { data } = await lastValueFrom(
+        this.httpService.post(`${apiUrl}`, params, {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+        }),
+      );
+
+      return data;
+    } catch (error) {
+      console.error('Error enviando whatsapp:', error);
+      return;
     }
   }
 }
