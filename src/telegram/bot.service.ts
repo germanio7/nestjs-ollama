b4b162c41934ee90de5f9cc37fd938f2b7f1a5b1
German Escobar <german.e.escobar@gmail.com>
2024-12-10 15:29:33 -0300

    merge

--- src/telegram/bot.service.ts
+++ src/telegram/bot.service.ts
@@ -1,15 +1,10 @@
 import { InjectQueue } from '@nestjs/bullmq';
 import { Injectable } from '@nestjs/common';
 import { Queue } from 'bullmq';
-import { ExternalApiService } from 'src/external-api/external-api.service';
-// import { Context } from 'telegraf';
 
 @Injectable()
 export class BotService {
-  constructor(
-    private readonly externalApiservice: ExternalApiService,
-    @InjectQueue('messageQueue') private messageQueue: Queue,
-  ) {}
+  constructor(@InjectQueue('messageQueue') private messageQueue: Queue) {}
 
   handleStart(ctx) {
     ctx.reply('Bienvenido');
